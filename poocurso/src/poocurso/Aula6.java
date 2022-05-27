a95b0800c8f4a22201dc2de721ad32e9d1dc2894
daniell wendyson <daniellwendyson@gmail.com>
2022-05-27 16:19:04 -0300

    aula 6

---
+++ poocurso/src/poocurso/Aula6.java
@@ -0,0 +1,18 @@
+package poocurso;
+
+public class Aula6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Aula6ControleRemoto c = new Aula6ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.play();
+		// c.ligarMudo();
+		c.abrirMenu();
+		c.fecharMenu();
+		
+		
+	}
+
+}
